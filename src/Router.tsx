774d407b2dee427051c60835da4099aca3b81c4b
Youn-Rha <ry0218@naver.com>
2025-03-25 21:41:05 +0900

    환경 세팅 수정정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage/MainPage";
-import BottomNav from "./components/common/BottomNav";
+import MainPage from "./pages/Main/MainPage";
+import BottomNav from "./components/shared/BottomNav";
+import Header from "./components/shared/Header";
 
 export const Router = () => {
     return (
         <>
+            <Header />
             <Routes>
                 <Route path="/" element={<MainPage />} />
             </Routes>
