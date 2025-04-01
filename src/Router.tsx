ebc06bfe1abcd25eeee5761992b6afecf5b24370
Youn-Rha <ry0218@naver.com>
2025-04-01 21:57:06 +0900

    회원가입 페이지 라우팅 추가가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import MainPage from "./pages/Main/MainPage";
 import BottomNav from "./components/shared/BottomNav";
 import Header from "./components/shared/Header";
+import SignUpPage from "./pages/SignUp/SignUpPage";
 
 export const Router = () => {
+    const location = useLocation();
+    const hideLayoutRoutes = ["/signup"];
+    const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
+
     return (
         <>
-            <Header />
+            {!shouldHideLayout && <Header />}
             <Routes>
                 <Route path="/" element={<MainPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
             </Routes>
-            <BottomNav />
+            {!shouldHideLayout && <BottomNav />}
         </>
     );
 };
